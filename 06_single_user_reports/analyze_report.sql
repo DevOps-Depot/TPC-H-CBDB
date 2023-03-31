fa80130d39f3f3a17c7da76cbd06bc8c0910989a
weiyi <weiyi@hashdata.cn>
2023-03-31 16:21:08 +0800

    optimze report output

--- 06_single_user_reports/analyze_report.sql
+++ 06_single_user_reports/analyze_report.sql
@@ -1,4 +1,4 @@
-SELECT split_part(description, '.', 1) as schema_name, extract('epoch' from duration) AS seconds 
+SELECT split_part(description, '.', 1) as schema_name, round(extract('epoch' from duration),1) AS seconds 
 FROM tpch_reports.sql 
 WHERE tuples = -1
 ORDER BY 1;
