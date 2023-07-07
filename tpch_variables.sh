a85afe2a2da0680b8b92d8f92902345cb05461ea
Ryan Wei <rainmanwy@gmail.com>
2023-07-07 17:29:28 +0800

    Change init parameter RUN_QGEN="true"

--- tpch_variables.sh
+++ tpch_variables.sh
@@ -56,7 +56,7 @@ export RUN_SINGLE_USER_REPORTS="true"
 
 # step 07_multi_user
 export RUN_MULTI_USER="false"
-export RUN_QGEN="false"
+export RUN_QGEN="true"
 
 # step 08_multi_user_reports
 export RUN_MULTI_USER_REPORTS="false"
