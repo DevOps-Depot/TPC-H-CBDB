7433e1bbefaa9bc8287e3f25cb591de5cea78220
Ryan Wei <rainmanwy@gmail.com>
2023-09-25 17:01:23 +0800

    bug fix

--- 03_ddl/rollout.sh
+++ 03_ddl/rollout.sh
@@ -15,6 +15,12 @@ filter="gpdb"
 schema_name=${SCHEMA_NAME}
 ext_schema_name="ext_${SCHEMA_NAME}"
 
+if [ "${VERSION}" == "gpdb_5" ] || [ "${VERSION}" == "gpdb_4_3" ]; then
+  distkeyfile="distribution_original.txt"
+else
+  distkeyfile="distribution.txt"
+fi
+
 if [ "${DROP_EXISTING_TABLES}" == "true" ]; then
   #Create tables
   for i in $(ls ${PWD}/*.${filter}.*.sql); do
@@ -26,7 +32,7 @@ if [ "${DROP_EXISTING_TABLES}" == "true" ]; then
     if [ "${RANDOM_DISTRIBUTION}" == "true" ]; then
       DISTRIBUTED_BY="DISTRIBUTED RANDOMLY"
     else
-      for z in $(cat ${PWD}/distribution.txt); do
+      for z in $(cat ${PWD}/${distkeyfile}); do
         table_name2=$(echo ${z} | awk -F '|' '{print $2}')
         if [ "${table_name2}" == "${table_name}" ]; then
           distribution=$(echo ${z} | awk -F '|' '{print $3}')
